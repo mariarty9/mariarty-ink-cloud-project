d97e5e6ef25dad7ba147176630cd91c605062ea3
mariarty9 <borysenko.mr31@gmail.com>
2026-08-13 21:34:13 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 //Single point of configuration for the API Gateway address.
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = 'http://70.156.229.163:30187';
 
 document.addEventListener('DOMContentLoaded', function() {
     //Dom elements
